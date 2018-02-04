ea3f02dbed3b685f9c1febdde478ca60dc7509f3
hanchaeyeon <cyhan1004@naver.com>
2018-02-04 18:37:15 +0900

    modify upload script

--- script/k_global_cuckoo.py
+++ script/k_global_cuckoo.py
@@ -19,14 +19,15 @@ ANALYZE_DIRECTORY = "/home/wins/not_analyze" # 쿠쿠 코어에 업로드할 파
 TARGET_DIRECTORY = "/home/wins/analyze" # 리포트파일 디렉토리(detect)
 WINS_IP = "112.168.216.205"
 WINS_PORT = 222
-PICKLE = "/home/wins/tasklist.pickle"
+PICKLE = "/home/wins/tasklist2.pickle"
 
 def detectUploadingFile(file_path = ANALYZE_DIRECTORY) :
 	file_list = []
 	filenames = os.listdir(file_path) #ANALYZE_DIRECTORY
 	for file in filenames:
+		print(os.path.join(file_path, file))
 		full_file_name = os.path.join(file_path, file)
-		file_list.append(full_filename)
+		file_list.append(full_file_name)
 	for path in file_list:
 		with open(path, "rb") as f:
 			file_name = os.path.basename(path)
@@ -36,7 +37,7 @@ def detectUploadingFile(file_path = ANALYZE_DIRECTORY) :
 			#file_name = path_split_list[-1]
 			fs = {"file": (file_name, f)}
 			r = requests.post(REST_URL, files=fs)
-			os.remove(os.path.join(path, file_name))
+			os.remove(path)
 	#threading.Timer(3600, detectUploadingFile, args=[file_path]).start()
 
 def detectReportFile(file_path = DIRECTORY_REPORT_ORIGINAL) :
@@ -54,48 +55,41 @@ def detectReportFile(file_path = DIRECTORY_REPORT_ORIGINAL) :
 	if len(current_set) == 0:
 		return
 	#	pickle.dump(set(task_list), f)
-	work_set = current_set - previous_set
+	work_set = current_set - previous_set #all files - completed files
 	complete_set = previous_set
+	pending_list = list() #uploading file list to wins server 
 	for task_num in work_set:
 		src_file_path = file_path +'/'+ task_num + "/reports/report.json"
 		if(os.path.exists(src_file_path)):
-			with open(src_file_path,'r') as f:
-				json_data = json.load(f)
-				md5 = json_data["target"]["file"]["md5"]
-				new_path = TARGET_DIRECTORY + "/" + md5 + ".json"
-				shutil.copy(src_file_path, new_path)	
-				complete_set.add(task_num)
+			pending_list.append([src_file_path,task_num])
+			print("add pending list :\n "+src_file_path)
+	success_set = transferFilesToServer(pending_list)
+	complete_set = complete_set | success_set
 	with open(PICKLE, 'wb') as f :
 		pickle.dump(complete_set, f)
 	print("finish")
 
-def transferFilesToServer(pasthList, destination):
+def transferFilesToServer(pending_list):
 	transport = paramiko.Transport((WINS_IP, WINS_PORT))
 	transport.connect(username="wins", password="wins!!!!")
 	sftp = paramiko.SFTPClient.from_transport(transport)
-	for name in os.listdir(pathList):
-		local_path = os.path.join(pathList, name)
-		remote_path = os.path.join(destination, name)
-		sftp.put(local_path, remote_path)
-		print(remote_path)
+	ret_set = set()
+	for file_path, task_num in pending_list:
+		with open(file_path,'r') as f:
+			json_data = json.load(f)
+			md5 = json_data["target"]["file"]["md5"]
+			new_path = TARGET_DIRECTORY + "/" + md5 + ".json"
+			try:
+				sftp.put(file_path, new_path)
+				ret_set.add(task_num)
+				#os.remove(file_path)	#로컬에 파일 남기기 싫으면 주석 해제
+			except Exception as e:
+				raise e	
 	sftp.close()
 	transport.close()
-
+	return ret_set
 
 if __name__ == '__main__' :
+	detectUploadingFile(ANALYZE_DIRECTORY)	
 	detectReportFile(DIRECTORY_REPORT_ORIGINAL)
 	exit()
-
-task_list = os.listdir(DIRECTORY_ORIGINAL)
-#for문 : 리포트를 새로운 디렉토리로 옮기는 로직
-for task in range(1,2): #task 번호만큼(ex : 1~ 30,000번 task의 리포트를 옮기고 싶으면 (1,30001)) => 수정
-	task_dir_name = DIRECTORY_REPORT_ORIGINAL + "/" + str(task) + "/reports/"
-	task_path = task_dir_name + "report.json"
-	try:
-		with open(task_path) as f:
-			json_data = json.load(f)
-			md5 = json_data["target"]["file"]["name"]
-			new_path = TARGET_DIRECTORY + "/" + md5 + ".json" 
-			transferFilesToServer(task_path,new_path)
-	except:
-		continue
