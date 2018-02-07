aaedbfc2fe298757d2d46f2abfeb4cba27a1b3a8
hanchaeyeon <cyhan1004@naver.com>
2018-02-07 18:38:08 +0900

    modify shellscript.sh - add removing pickle file process

--- script/shellscript.sh
+++ script/shellscript.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-#sudo cuckoo clean
 sudo cuckoo &
+sleep 20
 sudo cuckoo process instance1 & sudo cuckoo process instance2 & sudo cuckoo process instance3 &
 sudo cuckoo web -H 203.246.112.138 -p 8080 &
 sudo cuckoo api -H 192.168.56.1 -p 8090 &
+sudo rm -rf /home/wins/tasklist.pickle
 sudo /usr/sbin/cron
+
